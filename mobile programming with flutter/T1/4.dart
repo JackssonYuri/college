96636ecfa6b526d131a3a16cdc5857489ed1fd7e
Jacksson Yuri <78384230+JackssonYuri@users.noreply.github.com>
2023-04-16 15:01:43 -0300

    Create 4.dart
    Crie um programa Dart que imprima números de 1 a 100 e exiba a
    informação se o número é par, se for o caso, por exemplo:
    Valor de i = 1
    Valor de i = 2 -> par
    Valor de i = 3
    Valor de i = 4 -> par
    ...

---
+++ mobile programming with flutter/T1/4.dart
@@ -0,0 +1,11 @@
+//Questão 4
+void main() {
+  for (int i = 1; i <= 100; i++) {
+    if (i % 2 == 0){
+      print("Valor de: ${i} -> par");
+    }else{
+      print("Valor de: ${i}");
+    }
+  
+}
+}
